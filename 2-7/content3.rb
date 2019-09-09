3d85d9640febc3323612c03217d5a88792527b8a
tatsuro-kyoizumi <tatsu.sd.us@gmail.com>
2019-09-09 16:48:28 +0900

    [Finish]2-7

---
+++ 2-7/content3.rb
@@ -0,0 +1,20 @@
+apple="Aomori"
+
+if apple=="Nagano"
+  puts "りんごの生産地は長野です"
+
+else
+  puts "りんごの生産地は青森です"
+end
+
+apple="Yamagata"
+
+if apple=="Nagano"
+  puts "りんごの生産地は長野です"
+
+elsif apple=="Yamagata"
+  puts "りんごの生産地は山形です"
+
+else
+  puts "りんごの生産地は青森です"
+end
